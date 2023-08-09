ce0f64178af05266e7d2166c27296bfdeb26530c
Cristian J. Ambrosi <cris_ambrosi@hotmail.com>
2023-08-08 22:41:10 -0300

    chore: add NextIntlClientProvider in AppProvider file

--- src/services/providers/AppProvider.tsx
+++ src/services/providers/AppProvider.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import { ReactNode } from 'react';
-import StyledComponentsRegistry from '@libs/SCRegistry';
+import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
 import { ThemeProvider } from 'styled-components';
+
+import StyledComponentsRegistry from '@libs/SCRegistry';
 import { theme } from '@themes/theme';
+
 import { useBoundStore } from '@services/stores/useBoundStore';
 
 interface IAppProvider {
   children: ReactNode;
+  locale?: string;
+  messages?: AbstractIntlMessages;
 }
 
-const AppProvider = ({ children }: IAppProvider): JSX.Element => {
+const AppProvider = ({
+  children,
+  locale,
+  messages
+}: IAppProvider): JSX.Element => {
   const { themeName } = useBoundStore((state) => state);
   const currentTheme = themeName === 'dark' ? theme.dark : theme.light;
 
   return (
-    <>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       <StyledComponentsRegistry>
         <ThemeProvider theme={currentTheme}>{children}</ThemeProvider>
       </StyledComponentsRegistry>
-    </>
+    </NextIntlClientProvider>
   );
 };
 
